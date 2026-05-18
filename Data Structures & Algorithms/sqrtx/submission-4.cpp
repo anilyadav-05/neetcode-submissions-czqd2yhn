ed2c84b116d0a4475e0f55a51047a1276f0d9171
anilyadav-05 <ay70677316@gmail.com>
2026-05-19 01:21:09 +0530

    Add: sqrtx - submission-4

---
+++ Data Structures & Algorithms/sqrtx/submission-4.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x<1) return 0;
+        if(x==1) return 1;
+
+        long long st = 1, end = x/2;
+        while(st<=end){
+            long long mid = st + (end - st)/2;
+            
+            if(mid*mid > x){
+                end = mid -1 ;
+            }else if(mid*mid < x){
+                st = mid +1;
+            }else{
+                return (int)mid;
+            }
+        }
+        return (int)end;
+    }
+};
