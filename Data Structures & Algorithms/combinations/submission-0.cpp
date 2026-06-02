602a0b090dd496227a0eb364c2924edf48e72d34
anilyadav-05 <ay70677316@gmail.com>
2026-06-02 11:36:10 +0530

    Add: combinations - submission-0

---
+++ Data Structures & Algorithms/combinations/submission-0.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+
+    void helper(vector<vector<int>>&ans, vector<int>&subans, int n, int k , int idx){
+
+        if(subans.size()==k){
+            ans.push_back(subans);
+        }
+        if(subans.size()>k) return;
+
+        for(int i=idx; i<=n; i++){
+            subans.push_back(i);
+            helper(ans, subans, n, k, i+1);   // recursion
+            subans.pop_back();                  // backtraking
+
+        }
+
+    }
+
+
+
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>>ans;
+        vector<int>subans;
+
+        helper(ans , subans, n , k , 1);
+
+return ans;
+    }
+};
