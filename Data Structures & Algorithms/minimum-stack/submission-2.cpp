52253dc728ef000247a234b8cab9e34d8c337dd7
Anil yadav <ay70677316@gmail.com>
2026-06-20 03:55:44 +0530

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.cpp
@@ -0,0 +1,27 @@
+class MinStack {
+    stack<int> s;
+    stack<int> minS;  // minS.top() = current minimum
+public:
+    MinStack() {}
+
+    void push(int val) {
+        s.push(val);
+        if (minS.empty())
+            minS.push(val);
+        else
+            minS.push(min(val, minS.top()));
+    }
+
+    void pop() {
+        s.pop();
+        minS.pop();
+    }
+
+    int top() {
+        return s.top();
+    }
+
+    int getMin() {
+        return minS.top();
+    }
+};
